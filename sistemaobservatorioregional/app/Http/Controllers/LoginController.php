137123fce495255524afa6366afc00506da341b5
YeiZu04 <yeilanzcalderon16@gmail.com>
2022-11-14 12:08:33 -0600

    arreglo del login

--- sistemaobservatorioregional/app/Http/Controllers/LoginController.php
+++ sistemaobservatorioregional/app/Http/Controllers/LoginController.php
@@ -8,32 +8,30 @@ use Termwind\Components\Dd;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('admin_layouts.auth.login');
     }
 
 
-    public function store(Request $request){
-       
-        $this -> validate(request(),[
+    public function store(Request $request)
+    {
+        
+        $this->validate(request(), [
             'email' => 'required|email',
             'password' => 'required'
         ]);
-
-       // me doy por vencido con esto, tengo que revisar porque no  se esta autenticando
-        if(!auth()->attempt(request(['email','password']))){
+   
+        if (!auth()->attempt(request(['email', 'password']))) {
             return back()->with('mensaje', 'Credenciales incorrectas');
         }
-        
-        $dimensionModel = new Dimension(); 
-        $dimensionModel = $dimensionModel->makeVisible(['dimension_id']); 
-        $data = [          
+
+        $dimensionModel = new Dimension();
+        $dimensionModel = $dimensionModel->makeVisible(['dimension_id']);
+        $data = [
             'dimensions' => $dimensionModel->getAllDimension()
         ];
-        
+
         return view('admin_layouts.dimension.manage', $data);
-        
     }
-
-
 }
